c065ae3ab44cea832e56e9c4d41af4da22d29853
Bendeguz Cseresnyes <cser.bende@gmail.com>
2023-11-24 19:04:07 +0100

    Fix demo_worker build

--- workers/socket_app.py
+++ workers/socket_app.py
@@ -1,9 +1,13 @@
 import logging
+import os
 import sys
 import traceback
 
 import socketio
+import time
 from worker_base import SocketModelHandler
+
+time.sleep(1)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,26 +17,30 @@ logging.basicConfig(
 
 
 module_name = sys.argv[1] if len(sys.argv) > 1 else "demo_worker.orchestrator"
-bff_url = sys.argv[2] if len(sys.argv) > 2 else "http://localhost:5001"
+bff_url = (
+    sys.argv[2]
+    if len(sys.argv) > 2
+    else os.environ["SCHEDULER_URL"]
+)
 
 module_import = __import__(module_name, fromlist=["Orchestrator"])
 
-print("Loading orchestrator")
+logging.info("Loading orchestrator")
 orchestrator = module_import.Orchestrator()
 
-print("Starting app in socket mode")
+logging.info("Starting app in socket mode")
 sio = socketio.Client()
 
 
 @sio.event
 def execute(data):
-    print(f"execution requested {data}")
+    logging.info(f"execution requested {data}")
     handler = SocketModelHandler(data, sio)
     try:
         orchestrator.execute(handler)
     except Exception as e:
-        print(f"Error occured s{type(e)}")
-        traceback.print_exc()
+        logging.info(f"Error occured s{type(e)}")
+        traceback.logging.info_exc()
         handler.send_text("<asset:error:>")
         handler.finalize()
     finally:
@@ -42,17 +50,17 @@ def execute(data):
 
 @sio.event
 def connect():
-    print("Connected to executor service")
+    logging.info("Connected to executor service")
 
 
 @sio.on("*")
 def handle_messages(event, *args):
-    print(f"Recieved message {event} {args}")
+    logging.info(f"Recieved message {event} {args}")
 
 
 @sio.event
 def disconnect():
-    print("Disconnected from executor service")
+    logging.info("Disconnected from executor service")
 
 
 sio.connect(bff_url)

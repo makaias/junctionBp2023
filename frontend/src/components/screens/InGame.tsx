f1899efbc332670a373f058af55151f401b89dcd
Attila Makai <sandorattilamakai@gmail.com>
2023-11-25 14:05:15 +0200

    message

--- frontend/src/components/screens/InGame.tsx
+++ frontend/src/components/screens/InGame.tsx
@@ -1,20 +1,36 @@
 import { useState } from "react";
 import { Progress } from "../progress/Progress";
+import { useAppActions } from "../../game/useAppActions";
+import useAppState from "../../game/useAppState";
 
 export const InGame = () => {
   const [userTurn, setUserTurn] = useState<boolean>(false);
+  const actions = useAppActions();
+  const appState = useAppState();
+
+  const calculateBackground = () => {
+    if (appState.game?.type === 'ROBERT') {
+      return 'url("/robert_bg_1.jpg")'
+    }
+    return 'url("/background.jpg")'
+  }
+
+  const exitGame = () => {
+    actions.exitGame();
+  };
+
   return (
     <div
       className="w-screen h-screen flex justify-center items-end p-4"
       style={{
-        backgroundImage: 'url("/background.jpg")',
+        backgroundImage: calculateBackground(),
         backgroundSize: "cover",
         boxSizing: "border-box",
       }}
     >
-      <div className="flex w-full max-w-2xl flex-col justify-center">
-        <div className="w-full justify-center flex">
-          <img className="w-60 h-60" src="/man.png" alt="" />
+      <div className="flex w-full max-w-5xl flex-col justify-center">
+        <div className="w-full justify-end flex">
+          <img className="" src={appState.game?.type === 'SARAH' ? '/sarah.png' : '/robert.png'} alt="" />
         </div>
         <div className="flex w-full gap-4">
           <div className="flex w-full flex-col">
@@ -25,8 +41,8 @@ export const InGame = () => {
                 <Progress />
               </div>
             </div>
-            <div className="w-full flex max-w-2xl bg-gray-200 p-4 opacity-90 h-40">
-              <div className="w-full">
+            <div className="w-full flex flex-col max-w-5xl bg-gray-200 p-4 opacity-90 h-40">
+              <div className="w-full h-full space-between">
                 {userTurn ? (
                   <div>
                     <textarea
@@ -36,14 +52,15 @@ export const InGame = () => {
                     />
                   </div>
                 ) : (
-                  <div className="overflow-y-scroll">
+                  <div className="overflow-y-scroll flex h-full">
                     <p>
                       reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
                     </p>
                   </div>
                 )}
               </div>
-              <div className="flex h-full items-end">
+              <div className="flex h-fit justify-between">
+                <button onClick={() => exitGame()}>Exit</button>
                 <button onClick={() => setUserTurn(!userTurn)}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
